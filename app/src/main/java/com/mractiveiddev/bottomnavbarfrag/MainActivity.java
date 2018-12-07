972df8745b0dfa8ced5c6b48ba8921457352328a
maulana2ahmad <maulana2ahmad@gmail.com>
2018-12-07 15:20:01 +0700

    update run emularot API28 succes

--- app/src/main/java/com/mractiveiddev/bottomnavbarfrag/MainActivity.java
+++ app/src/main/java/com/mractiveiddev/bottomnavbarfrag/MainActivity.java
@@ -1,77 +1,108 @@
 package com.mractiveiddev.bottomnavbarfrag;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView mMainNav;
-    private FrameLayout mMainFrame;
-
-    private HomeFragment homeFragment;
-    private MallFragment mallFragment;
-    private NotificationFragment notificationFragment;
-    private TransactionFragment transactionFragment;
-    private AccountFragment accountFragment;
+//    private BottomNavigationView mMainNav;
+//    private FrameLayout mMainFrame;
+//
+//    private HomeFragment homeFragment;
+//    private MallFragment mallFragment;
+//    private NotificationFragment notificationFragment;
+//    private TransactionFragment transactionFragment;
+//    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
-        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        homeFragment = new HomeFragment();
-        mallFragment = new MallFragment();
-        notificationFragment = new NotificationFragment();
-        transactionFragment = new TransactionFragment();
-        accountFragment = new AccountFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_bottom, new HomeFragment()).commit();
 
-
-        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_home:
-                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
-                        setFragment(homeFragment);
-                        return true;
-                    case R.id.nav_mall:
-                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
-                        setFragment(mallFragment);
-                        return true;
-                    case R.id.nav_notif:
-                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
-                        setFragment(notificationFragment);
-                        return true;
-                    case R.id.nav_transaction:
-                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
-                        setFragment(transactionFragment);
-                        return true;
-                    case R.id.nav_account:
-                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
-                        setFragment(accountFragment);
-                        return true;
-
-                    default:
-                        return false;
-                }
+//        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+//        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+//
+//        homeFragment = new HomeFragment();
+//        mallFragment = new MallFragment();
+//        notificationFragment = new NotificationFragment();
+//        transactionFragment = new TransactionFragment();
+//        accountFragment = new AccountFragment();
+//
+//        setFragment(homeFragment);
+//
+//
+//        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.nav_home:
+//                      //  mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+//                        setFragment(homeFragment);
+//                        return true;
+//                    case R.id.nav_mall:
+//                      //  mMainNav.setItemBackgroundResource(R.color.colorAccent);
+//                        setFragment(mallFragment);
+//                        return true;
+//                    case R.id.nav_notif:
+//                      //  mMainNav.setItemBackgroundResource(R.color.colorAccent);
+//                        setFragment(notificationFragment);
+//                        return true;
+//                    case R.id.nav_transaction:
+//                        //mMainNav.setItemBackgroundResource(R.color.colorAccent);
+//                        setFragment(transactionFragment);
+//                        return true;
+//                    case R.id.nav_account:
+//                       // mMainNav.setItemBackgroundResource(R.color.colorAccent);
+//                        setFragment(accountFragment);
+//                        return true;
+//
+//                    default:
+//                        return false;
+//                }
+//            }
+//        });
+//
+//    }
+//
+//    private void setFragment(Fragment fragment) {
+//
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.main_frame, fragment);
+//        fragmentTransaction.commit();
+//
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_mall:
+                    selectedFragment = new MallFragment();
+                    break;
+                case R.id.nav_notif:
+                    selectedFragment = new NotificationFragment();
+                    break;
+                case R.id.nav_transaction:
+                    selectedFragment = new TransactionFragment();
+                    break;
+                case R.id.nav_account:
+                    selectedFragment = new AccountFragment();
+                    break;
             }
-        });
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_bottom, selectedFragment).commit();
 
-    }
-
-    private void setFragment(Fragment fragment) {
-
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame, fragment);
-        fragmentTransaction.commit();
-
-    }
+            return true;
+        }
+    };
 }
